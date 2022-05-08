bb9dfee16a4e0c7c913a418c8aa6a15bcee2743d
brend3n <brenden@knights.ucf.edu>
2022-05-08 19:20:46 -0400

    Added some more code

--- random_identity.py
+++ random_identity.py
@@ -16,10 +16,27 @@ states = [ 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
            'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX',
            'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
 
+
 class Random_Identity:
     def __init__(self):
+        self.get_name()
+        self.get_email()
+        self.get_phone_number()
+        self.get_random_state()
+        self.get_random_city()
+        self.get_address()
         return
     
+    def to_string(self):
+        print(f"First Name: {self.first_name}\n
+                Last Name: {self.last_name}\n
+                Phone Number: {self.phone_number}\n
+                Email: {self.email}\n
+                State: {self.state}\n
+                City: {self.city}\n
+                Address: {self.address}\n")
+
+
     # TODO: Change this so it is 10 minute temporary phone number or longer
     def get_phone_number(self):
         state = self.state
@@ -32,20 +49,24 @@ class Random_Identity:
         self.phone_number = phone_number
 
     # TODO: Change this to use the other API
-    def get_email():
+    def get_email(self):
         return
 
-    def get_random_state():
+    def get_random_state(self):
         self.state = random.choice(states)
 
-    def get_random_city():
+    def get_random_city(self):
         url = f"https://locations.chipotle.com/{str(state).lower()}"
         soup = get_soup_adv(url)
         cities = soup.find_all("a", "Directory-listLink")
         self.city = random.choice(cities)['href']
 
-    def get_name():
-        self.first_name = random_first_name_updated(path="./Random-Name-Generator")
-        self.last_name = random_last_name_updated(path="./Random-Name-Generator")
+    # TODO: Finish this function
+    def get_address():
+        city = self.city
+        state = self.state
+        pass
 
-
+    def get_name(self):
+        self.first_name = random_first_name_updated(path="./imports//Random-Name-Generator")
+        self.last_name = random_last_name_updated(path="./imports/Random-Name-Generator")

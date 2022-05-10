0ff0556d1921e9734aacefd954b72a41ea965c10
brend3n <brenden@knights.ucf.edu>
2022-05-09 21:23:14 -0400

    Removed some unused code

--- api.py
+++ api.py
@@ -1,19 +1,10 @@
-from typing import Optional
 from fastapi import FastAPI
-#from random_identity import Random_Identity, BackgroundTasks
 from random_identity import Random_Identity
-import json
-from time import sleep
+
 app = FastAPI()
 
-def get_identity():
-    data = json.dumps(Random_Identity.to_json())
-    return data
-
 @app.get("/")
-# async def all(background_tasks: BackgroundTasks):
 async def all():
-    # background_tasks.add_task(get_identity)
     data = Random_Identity().to_json()
     return data
 

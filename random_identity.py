d6587b79a2de2d726cd65848afc34eb05cfc7f54
brend3n <brenden@knights.ucf.edu>
2022-05-10 20:08:25 -0400

    Email is working now with pymailtm api

--- random_identity.py
+++ random_identity.py
@@ -8,10 +8,12 @@ sys.path.insert(0,"./imports")
 sys.path.insert(0,'./imports/Random-Name-Generator')
 sys.path.insert(0,'./imports/web_scraper')
 sys.path.insert(0,'./imports/myTempEmail-API')
+sys.path.insert(0,'./imports/pymailtm/pymailtm')
 
 from random_name import random_first_name_updated, random_last_name_updated
 from bs4 import BeautifulSoup
 from web_scraper import get_soup_adv
+from pymailtm import MailTm
 
 urllib3.disable_warnings()
 
@@ -28,9 +30,12 @@ class Random_Identity:
         self.state = "TODO"
         self.city = "TODO"
         self.get_name()
-        self.get_email()
         self.get_address()
         self.get_phone_number()
+        self.get_email()
+        print(self.to_json())
+        print(f"\nEmail: {self.emailaddress}\nPassword: {self.emailpassword}")
+        print(f"\nGo to https://mail.tm/en/. Sign-out and then sign-in using credentials above.\n\n")
         return
     
     def to_string(self):
@@ -42,7 +47,8 @@ class Random_Identity:
             "First Name" : self.first_name,
             "Last Name" : self.last_name,
             "Phone Number": self.phone_number,
-            "Email" : self.email,
+            "Email" : self.emailaddress,
+            "Email Password": self.emailpassword,
             "State": self.state,
             "City" : self.city,
             "Address" : self.address
@@ -61,7 +67,10 @@ class Random_Identity:
 
     # TODO: Change this to use the other API
     def get_email(self):
-        self.email = "TODO"
+        mail_obj = MailTm().get_account()
+        self.emailaddress = mail_obj.address
+        self.emailpassword  = mail_obj.password
+        
 
     def get_address(self):
         
@@ -76,4 +85,5 @@ class Random_Identity:
         self.last_name = random_last_name_updated(path="./imports/Random-Name-Generator/")
 
 person = Random_Identity()
-print(person.to_json())
+
+
